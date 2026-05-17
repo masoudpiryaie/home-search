68005b3a1b1313921d0cace555b681f24ef680aa
masoud <fornt.react.dev@gmail.com>
2026-05-17 20:23:51 +0200

    change default language to fa2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const locales = ["en", "fa", "de"];
-const defaultLocale = "en";
+const defaultLocale = "fa";
 
 function pathnameHasLocale(pathname: string) {
   return locales.some(
